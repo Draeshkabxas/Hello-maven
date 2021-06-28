d28b5f9be958cfcdbd14d30dd18aeccfaefe2bed
Drar <zzxxccz908@gmail.com>
2021-06-28 09:10:40 +0200

    Add to log for test if our logger work correctly.

--- src/main/java/academy/learnprogramming/HelloMaven.java
+++ src/main/java/academy/learnprogramming/HelloMaven.java
@@ -1,7 +1,16 @@
 package academy.learnprogramming;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class HelloMaven {
+
+    //Add logger
+    private final static Logger log=
+            LoggerFactory.getLogger(HelloMaven.class);
     public static void main(String[] args) {
         System.out.println("Hello Maven");
+        log.info("Hello info");
+        log.debug("Hello debug");
     }
 }
